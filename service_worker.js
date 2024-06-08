3f8cc9c028b8bcf244df4dcc6a9022b0b52b7258
Kingston Fung <kingstonfung@Kingstons-16-i7-MacBook-Pro.local>
2024-06-08 15:05:57 -0600

    Turning the extension to be manifest v3 compliant
    Plus adding a silly click tracking (clean hit, no user info passed) for my own analytic

---
+++ service_worker.js
@@ -0,0 +1,27 @@
+chrome?.action?.onClicked.addListener(async () => {
+  // Minimal efforts to hide my endpoint from the public
+  const path = "acmy6cxf7740qoqo0snot5wy2fush6fh".split('').reverse().join('');
+  const host = "moc.ekam.1su.kooh".split('').reverse().join('');
+
+  try {
+    const chromeTabs = chrome?.tabs;
+    const chromeWindows = chrome?.windows;
+    const bypassCacheSetting = { bypassCache: true };
+    const querySettings = { windowId: chromeWindows.WINDOW_ID_CURRENT };
+    
+    chromeTabs.query(querySettings, tabsList => {
+      tabsList.forEach(
+        ({ id }) => chromeTabs.reload(id, bypassCacheSetting),
+      );
+    });
+
+    // Just a +1 counter endpoint, to see how many clicks this tool receives.
+    fetch(`https://${host}/${path}`, { 
+      method: "GET",
+      headers: {}, // I am not passing any header informations
+      credentials: "omit", // I am not passing any cookie informations
+    });
+  } catch (e) {
+    console.error(e);
+  }
+});
