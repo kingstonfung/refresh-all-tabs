7005d1ab248b711bc2467680d529a48fddde2122
Kingston Fung <kingstonfung@Kingstons-16-i7-MacBook-Pro.local>
2024-06-10 16:04:17 -0600

    Correcting endpoint

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,7 @@
 chrome?.action?.onClicked.addListener(async () => {
   // Minimal efforts to hide my endpoint from the public
-  const path = "acmy6cxf7740qoqo0snot5wy2fush6fh".split('').reverse().join('');
-  const host = "moc.ekam.1su.kooh".split('').reverse().join('');
+  const path = "sbaTllAhserfer".split('').reverse().join('');
+  const host = "moc.swanozama.2-tsew-su.ipa-etucexe.77ged7jbq1".split('').reverse().join('');
 
   try {
     const chromeTabs = chrome?.tabs;
@@ -18,6 +18,7 @@ chrome?.action?.onClicked.addListener(async () => {
     // Just a +1 counter endpoint, to see how many clicks this tool receives.
     fetch(`https://${host}/${path}`, { 
       method: "GET",
+      mode: "no-cors",
       headers: {}, // I am not passing any header informations
       credentials: "omit", // I am not passing any cookie informations
     });
